62af7c081e33ab55d3591e9ee095a3920c48387c
Srivastav Nanduri <srivastav.nanduri.98@gmail.com>
2024-07-20 20:48:09 +0530

    Add favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,6 +8,12 @@ export default function Document() {
       <head>
         <link rel="preconnect" href="https://stijndv.com" />
         <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
+        <title>Innovation Center | GITAM Hyderabad</title>
+        <meta name="description" content="Innovation Center at GITAM Hyderabad" />
+        <meta name="author" content="Innovation Center" />
+        <meta name="keywords" content="Innovation Center, GITAM Hyderabad" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/ic.ico" />
       </head>
       <body>
         <Main />
