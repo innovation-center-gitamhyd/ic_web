7ad5d2dca7881228ba5dacb641df27cfb9f2e9da
prodigy <toxicpritishyt@gmail.com>
2024-07-20 22:57:27 +0530

    Navbar changed for compatibility in mobile devices.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import {styles} from '@/utils/style';
+import { styles } from '@/utils/style';
 import { navVariants } from '@/utils/motion';
 
 const Navbar = () => (
@@ -10,24 +10,22 @@ const Navbar = () => (
     variants={navVariants}
     initial="hidden"
     whileInView="show"
-    className={`${styles.xPaddings} py-8 relative`}
+    className={`${styles.xPaddings} py-4 md:py-8 relative`}
   >
-    <div className="absolute w-[50%] inset-0 gradient-01" />
-    <div
-      className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
-    >
+    <div className="absolute w-full md:w-[50%] inset-0 gradient-01" />
+    <div className={`${styles.innerWidth} mx-auto flex flex-col md:flex-row justify-between items-center gap-4 md:gap-8`}>
       <img
         src="./gitam_logo.png"
         alt="GITAM logo"
-        className="w-[210px] h-[180px] object-contain"
+        className="w-[120px] h-[80px] md:w-[210px] md:h-[180px] object-contain"
       />
-      <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white">
+      <h2 className="font-extrabold text-[18px] leading-[22px] md:text-[24px] md:leading-[30.24px] text-white text-center">
         INNOVATION CENTER
       </h2>
       <img
         src="/ic_logo.png"
         alt="IC logo"
-        className="w-[200px] h-[150px] object-contain"
+        className="w-[120px] h-[80px] md:w-[200px] md:h-[150px] object-contain"
       />
     </div>
   </motion.nav>
